f4820e457dfba434043b92929e4058b658a9a1fa
xuehua-snow <2502653036@qq.com>
2025-12-03 02:28:59 -0500

    add templates

--- app.py
+++ app.py
@@ -6,5 +6,34 @@ app = Flask(__name__)
 def start():
     return render_template("start.html")
 
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/citylist")
+def citylist():
+    return render_template("citylist.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
