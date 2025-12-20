7d7bd23b543a0ba633728c85b4febcdb914056b5
xuehua-snow <2502653036@qq.com>
2025-12-20 11:37:47 -0500

    v0.0.1

--- app.py
+++ app.py
@@ -1,36 +1,285 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect,session,jsonify
+from dotenv import load_dotenv
+import os
+import mysql.connector
 
-app = Flask(__name__)
+load_dotenv()
 
+app = Flask(__name__) 
+
+app.secret_key = os.getenv('SECRET_KEY')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
+def get_db():
+    return mysql.connector.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        database=app.config['MYSQL_DB']
+    )
+
+# --------------------------route-------------------------------
 @app.route("/")
 def start():
     return render_template("start.html")
-
-@app.route("/base")
-def base():
-    return render_template("base.html")
 
 @app.route("/homepage")
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/citylist")
-def citylist():
-    return render_template("citylist.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+@app.route("/cityslist")
+def cityslist():
+    return render_template("cityslist.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+# --------------------------city route-------------------------------
+@app.route("/cityslist/toronto")
+def toronto():
+    # --------------SQL----------------
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
 
+    if "user_id" in session:
+        userid = session["user_id"]
+        city = "toronto"
 
+        note_sql = '''
+                        SELECT content,id
+                        FROM note
+                        WHERE user_id = %s AND city = %s;
+                    '''
+        note_values = (userid, city)
+        
+        cursor.execute(note_sql, note_values)
+        note = cursor.fetchall()
+    else:
+        note = set()
 
+    cursor.close()
+    conn.close()
+    # ----------SQL End----------------  
 
+    return render_template("cities/toronto.html", note=note)
 
+@app.route("/cityslist/seulo")
+def seulo():
+    # --------------SQL----------------
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+
+    if "user_id" in session:
+        userid = session["user_id"]
+        city = "seulo"
+    
+        note_sql = '''
+                        SELECT content,id
+                        FROM note
+                        WHERE user_id = %s AND city = %s;
+                    '''
+        note_values = (userid, city)
+        
+        cursor.execute(note_sql, note_values)
+        note = cursor.fetchall()
+    else:
+        note = set()
+
+    cursor.close()
+    conn.close()
+    # ----------SQL End----------------  
+
+    return render_template("cities/seulo.html",note=note)
+
+@app.route("/cityslist/paris")
+def paris():
+    # --------------SQL----------------
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+
+    if "user_id" in session:
+        userid = session["user_id"]
+        city = "paris"
+    
+        note_sql = '''
+                        SELECT content,id
+                        FROM note
+                        WHERE user_id = %s AND city = %s;
+                    '''
+        note_values = (userid, city)
+        
+        cursor.execute(note_sql, note_values)
+        note = cursor.fetchall()
+    else:
+        note = set()
+
+    cursor.close()
+    conn.close()
+    # ----------SQL End---------------- 
+
+    return render_template("cities/paris.html",note=note)
+# -----------------------------form page------------------------------------
+# ------------------search city form---------------------
+# No city found page
+@app.route("/no")
+def no():
+    return render_template("/form/nocity.html")
+
+# Search city form 
+@app.route("/searchcity", methods=["POST"])
+def searchcity():
+    search = request.form.get('search').strip()
+    search_lower = search.lower()
+
+    city_list = ["toronto","seulo","paris"]   # 全部小写存储
+
+    if search_lower in city_list:
+        return redirect(f"/cityslist/{search_lower}")
+
+    else:
+        return render_template("form/nocity.html", search=search)
+
+# ----------------------sign up form---------------------
+@app.route('/signup', methods=['POST'])
+def signup():
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # --------------SQL----------------
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+
+    signup_sql = '''
+                   INSERT INTO `users` (username,password)
+                   VALUES (%s, %s)
+                   '''
+    signup_values = (username,password)
+
+    try:
+        cursor.execute(signup_sql, signup_values)
+        conn.commit()
+    except mysql.connector.Error as e:
+        print(e)
+    
+    cursor.close()
+    conn.close()
+    # ----------SQL End----------------
+
+    return redirect("/")
+
+# --------------------------login form-------------------------
+@app.route('/login', methods=['POST'])
+def login():
+
+    username = request.form.get('logusername')
+    password = request.form.get('logpassword')
+
+    # --------------SQL----------------
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+
+    login_sql = '''
+                    SELECT id, password, admin
+                    FROM users
+                    WHERE username = %s;
+                '''
+    login_values = (username,)
+
+    cursor.execute(login_sql, login_values)
+    currentUser = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+    # ----------SQL End----------------
+
+    if currentUser == None:
+        return "No"
+    else:
+        if password != currentUser["password"]:
+            return "wrong"
+        else:
+            session["user_id"] = currentUser["id"]
+            session["username"] = username
+            session["logged_in"] = True
+
+            return username
+
+# --------------------------log out form-------------------------
+@app.route('/logout', methods=['POST'])
+def logout():
+
+    session.clear()
+
+    return redirect("/")
+
+# --------------------------Add Note-------------------------
+@app.route('/addnote', methods=['POST'])
+def addnote():
+    content = request.form.get('content')
+    city = request.form.get('city')
+    country = request.form.get('country')
+    user_id = session["user_id"]
+
+    # --------------SQL----------------
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+
+    addnote_sql = '''
+                   INSERT INTO `note` (user_id,city,country,content)
+                   VALUES (%s, %s,%s,%s)
+                   '''
+    addnote_values = (user_id,city,country,content)
+
+    try:
+        cursor.execute(addnote_sql, addnote_values)
+        conn.commit()
+    except mysql.connector.Error as e:
+        print(e)
+    
+    cursor.close()
+    conn.close()
+    # ----------SQL End----------------
+
+    return redirect(request.referrer or '/')
+
+# --------------------------Edit Note-------------------------
+@app.route('/editnote', methods=['POST'])
+def editnote():
+    content = request.form.get('content')
+    city = request.form.get('city')
+    note_id = request.form.get('note_id')
+    user_id = session["user_id"]
+    
+
+    # --------------SQL----------------
+    conn = get_db()
+    cursor = conn.cursor(dictionary=True)
+
+    editnote_sql = '''
+                   UPDATE `note` 
+                   SET `content` = %s
+                   WHERE id = %s AND user_id = %s AND city = %s;
+                   '''
+    editnote_values = (content,note_id,user_id, city)
+    print('VALUES:', editnote_values)
+    try:
+        cursor.execute(editnote_sql, editnote_values)
+        print('ROWCOUNT:', cursor.rowcount)
+        conn.commit()
+        print('EDIT NOTE CALLED')
+    except mysql.connector.Error as e:
+        print(e)
+    
+    cursor.close()
+    conn.close()
+    # ----------SQL End----------------
+
+    return redirect(request.referrer or '/')
 
 
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,40 +1,86 @@
-// ----------------------Login/Sign Up Function----------------------------------
-    const dlg = document.getElementById('loginDialog');
-    document.getElementById('openLogin').addEventListener('click', () => dlg.showModal());
-    document.getElementById('closeLogin').addEventListener('click', () => dlg.close());
-    // 是否需要加一个X的关闭按钮？
-    // 登录是使用email还是别的自由命名？目前必须使用email登录
-    // ※注意一下这里的提交表单是否需要防止重定向
+// -----------------------login interface---------------------------------------------------
+const logindlg = document.getElementById('loginDialog');
+const openLogin = document.getElementById('openLogin');
+const SignUpdlg = document.getElementById('SignUpDialog');
 
-// ----------------------Map Function--------------------------------------------
-    // mapboxgl.accessToken = 'pk.eyJ1IjoiaDg2OTY2MTM2OCIsImEiOiJjbWgweXdiYXIyam4zMmpxMm4xdzVxdDZ3In0.ss474gDeBh40T0_8FFKw2A';
+if (openLogin) {
+    openLogin.addEventListener('click', () => logindlg.showModal());
+    document.getElementById('closeLogin').addEventListener('click', () => logindlg.close());
+    document.getElementById('openSignIn')
+            .addEventListener('click', () => {
+                                            SignUpdlg.close()
+                                            logindlg.showModal()
+                                            });
 
-    // const map = new mapboxgl.Map({
-    //   container: 'map',
-    //   style: 'mapbox://styles/mapbox/streets-v12', // 样式ID
-    //   center: [-79.3832, 43.6532], // 多伦多坐标
-    //   zoom: 12
-    // });
-
-    // // Add map scale handle 添加缩放控件
-    // map.addControl(new mapboxgl.NavigationControl());
-
-    // // Add Marker 添加标记
-    // new mapboxgl.Marker().setLngLat([-79.3832, 43.6532]).addTo(map);
-
-// -----------------------Note----------------------------------------------------
-    // 目前的架构到底是跟着flask项目结构做还是跟着原来的js文件夹结构做？
-    // ※ ※ ※记得把导航做成模板！！！！！！！
-    // 首页到底叫homepage还是index？
-    // start的颜色不对，需要吸色调整
+    document.getElementById('openSignup')
+            .addEventListener('click', () => {
+                                            logindlg.close()
+                                            SignUpdlg.showModal()
+                                            console.log("clicked")
+                                        });
+    document.getElementById('closeSignUp').addEventListener('click', () => SignUpdlg.close());
+}
 
 
+// -----------------------login Ajax---------------------------------------------------
+const loginForm = document.getElementById("loginForm");
+const errortint = document.getElementById("errortint");
 
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formData = new FormData(loginForm);
 
+    // -----------------Ajax------------------------------
+    let xhr = new XMLHttpRequest(); 
+    xhr.onreadystatechange = handlerFunction; 
 
-// --------------------------call UI-----------------------------------
-// 设计一个登录页面
-    // 登录和注册是同一个按钮和页面吗？
-// 登录图标旁边的用户头像，在没登陆前哪来的登录头像？需要设计登录前和登陆后的区别显示（※ 代码也需要设置此功能！！！）
-// -------------------------call Art-------------------------------------
-    // need a new Icon
+    function handlerFunction (event){     
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if (xhr.status === 200) {
+                if (xhr.responseText == "No"){
+                    errortint.textContent = "This username does not exist."
+                }
+                else if(xhr.responseText == "wrong"){
+                    errortint.textContent = "Incorrect password. Please try again."
+                }
+                else{
+                    document.location.href = "/";
+                }
+            } else {
+                console.log(xhr.status)
+            }
+        }
+    };
+
+    xhr.open("POST","/login",true); 
+    xhr.send(formData);
+    // -----------------Ajax finish-----------------------
+});
+
+// -----------------------Exit session---------------------------------------------------
+const menu = document.getElementById("menuBox");
+const userarea = document.getElementById("userarea");
+
+if (userarea && menu) {
+    userarea.addEventListener("mouseenter", () => {
+        menu.style.display = "block";
+    });
+    userarea.addEventListener("mouseleave", () => {
+        menu.style.display = "none";
+    });
+}
+// ---------------- Cookie Banner Close ----------------
+document.addEventListener("DOMContentLoaded", function () {
+    const banner = document.getElementById("cookieBanner");
+    const acceptBtn = document.getElementById("cookieAccept");
+    const ManageBtn = document.getElementById("cookieManage");
+
+    acceptBtn.addEventListener("click", function (e) {
+        e.preventDefault();               
+        banner.style.display = "none";    
+    });
+    ManageBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        banner.style.display = "none";
+    });
+});
